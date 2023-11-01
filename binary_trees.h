0c929ab16940b7b39aab3693ba5712e0cec26680
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-11-02 00:26:48 +0100

    header file\

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -26,6 +27,10 @@ typedef struct binary_tree_s binary_tree_t;
 /* functions */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(
+	binary_tree_t *parent,
+	int value
+);
 
 
 #endif /* BINARY_TREES_H */
