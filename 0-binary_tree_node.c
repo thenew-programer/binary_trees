a84f53ef78dd8a9d34e559dbe099b168f53cc6b2
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-11-02 00:18:21 +0100

    first task: create a child node of the parent node given

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,6 +13,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node =
 		(binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (!node)
+		return (NULL);
 	node->parent = parent;
 	node->left = NULL;
 	node->right = NULL;
