47a6f0bb317aebd5b4f2bd1e2101cc33e37a5eef
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-11-02 00:49:13 +0100

    header file

--- binary_trees.h
+++ binary_trees.h
@@ -31,6 +31,10 @@ binary_tree_t *binary_tree_insert_left(
 	binary_tree_t *parent,
 	int value
 );
+binary_tree_t *binary_tree_insert_right(
+	binary_tree_t *parent,
+	int value
+);
 
 
 #endif /* BINARY_TREES_H */
