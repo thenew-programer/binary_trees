74673969a8a3c79aba4c58121f8a7af987f1e094
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-11-03 00:25:03 +0100

    task 14: return the count of the nodes with at least 1 child in a binary tree

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,6 +2,23 @@
 
 
 /**
+ * binary_tree_is_leaf - check whether a node is a leaf or not
+ *
+ * @node: pointer to the node to check
+ *
+ * Return: 1 on success 0 on failure
+*/
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (node->left || node->right)
+		return (0);
+	return (1);
+}
+
+
+/**
  * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
  *
  * @tree: pointer to the root node of the tree
