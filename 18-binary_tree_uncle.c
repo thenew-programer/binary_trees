d5e7ffb5faf11b74d80d831c3a55f26d6203df53
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-11-03 01:46:16 +0100

    task 19: finds the uncle of a node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -12,8 +12,8 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent->parent || !node->parent)
 		return (NULL);
-	if (node->parent == node->parent->parent->right)
-		return (node->parent->parent->left);
-	return (node->parent->parent->right);
+	if (node->parent == node->parent->parent->left)
+		return (node->parent->parent->right);
+	return (node->parent->parent->left);
 
 }
